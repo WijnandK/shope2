0b8c67856cd2ae574e16053b858e3d065ee058b0
Wijnand Korlaar <wijniejs@gmail.com>
2021-02-11 19:10:46 +0100

    Ui update

--- src/store/reducers/product.js
+++ src/store/reducers/product.js
@@ -1,12 +1,14 @@
 import * as actionTypes from '../actions/actionsTypes';
  
-const initState = {products: [], product: {} }
+const initState = {products: [], product: {} , error: false}
 const reducer = ( state = initState, action) => {
     switch(action.type){
        case actionTypes.SET_PRODUCTS:
          return {...state, products: action.products}
            case actionTypes.SET_DETAIL_STATE:
          return {...state, product: action.product}
+          case actionTypes.SET_PRODUCTS_FAIL: 
+        return {...state, error: true}
         default:
             return state;
     }
